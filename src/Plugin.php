97be9ded6fd6e4800a47e60255713c2eec1f5e35
Edson Galina Fortes <edson@weglot.com>
2025-09-23 13:19:43 +0200

    remove unused autoload

--- src/Plugin.php
+++ src/Plugin.php
@@ -70,7 +70,6 @@ class Plugin extends BasePlugin
      */
     public function init(): void
     {
-        //require_once $this->getBasePath() . '/../vendor/autoload.php';
         parent::init();
         Craft::setAlias('@weglot/craftweglot', $this->getBasePath());
         $this->attachEventHandlers();
